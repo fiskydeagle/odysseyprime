6bb5e2eef8d6fcf5be3366aaf84a0941b0e75793
DESKTOP-MJPQ0T3\fisni <fiskydeagle@gmail.com>
2017-03-09 20:11:02 +0100

    Installation of Bootstrap framework Implementation of RouterModule for paths Setting up the Welcome Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpModule} from '@angular/http';
+import {RouterModule} from "@angular/router";
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {WelcomeComponent} from "./home/welcome.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        WelcomeComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpModule,
+        RouterModule.forRoot([
+            {path: 'welcome', component: WelcomeComponent, data: {title: 'Welcome'}},
+            {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+            {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+        ])
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/home/welcome.component.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by fisni on 09-Mar-17.
+ */
+import {Component} from '@angular/core';
+
+@Component({
+    templateUrl: './welcome.component.html'
+})
+export class WelcomeComponent {
+    public pageTitle: string = 'Welcome';
+}
